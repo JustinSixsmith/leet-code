b9b3ea22a939fca75fcd2319757062cc49f806be
Justin Sixsmith <justinsixsmith@gmail.com>
2022-06-19 09:51:44 -0500

    setting up an alternative method for int to english words

--- src/challenges/Solutions.java
+++ src/challenges/Solutions.java
@@ -99,4 +99,56 @@ public class Solutions {
     }
 
 
+    private String oneDigit(int num) {
+        switch (num) {
+            case 1: return "One";
+            case 2: return "Two";
+            case 3: return "Three";
+            case 4: return "Four";
+            case 5: return "Five";
+            case 6: return "Six";
+            case 7: return "Seven";
+            case 8: return "Eight";
+            case 9: return "Nine";
+        }
+        return "";
+    }
+
+    private String underTwenty(int num) {
+        switch (num) {
+            case 10: return "Ten";
+            case 11: return "Eleven";
+            case 12: return "Twelve";
+            case 13: return "Thirteen";
+            case 14: return "Fourteen";
+            case 15: return "Fifteen";
+            case 16: return "Sixteen";
+            case 17: return "Seventeen";
+            case 18: return "Eighteen";
+            case 19: return "Nineteen";
+        }
+    }
+
+    private String tens(int num) {
+        switch (num) {
+            case 20: return "Twenty";
+            case 30: return "Thirty";
+            case 40: return "Forty";
+            case 50: return "Fifty";
+            case 60: return "Sixty";
+            case 70: return "Seventy";
+            case 80: return "Eighty";
+            case 90: return "Ninety";
+        }
+    }
+
+    private String twoDigits(int num) {
+        if (num < 10) {
+            return oneDigit(num;
+        }
+        if (num < 20) {
+            return underTwenty(num);
+        }
+}
+
 }
