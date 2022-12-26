1ff08355566cef5d36ff3fba5ab870a5eeb5514b
Justin Sixsmith <justinsixsmith@gmail.com>
2022-12-26 16:57:18 -0600

    created a test case in main

--- src/easy/ArraySum.java
+++ src/easy/ArraySum.java
@@ -1,9 +1,10 @@
 package easy;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ArraySum {
-    public int[] runningSum(int[] nums) {
+    public static int[] runningSum(int[] nums) {
         int[] sums = new int[nums.length];
         int sum = 0;
         for (int i = 0; i < nums.length; i++) {
@@ -14,6 +15,7 @@ public class ArraySum {
     }
 
     public static void main(String[] args) {
-
+        int[] nums = {1, 2, 3, 4};
+        System.out.println(Arrays.toString(ArraySum.runningSum(nums)));
     }
 }
