cc2d33888f37fd6da88b52694aa031b4251f8626
Justin Sixsmith <justinsixsmith@gmail.com>
2022-12-27 11:19:53 -0600

    breaks out of loops if bags are all full

--- src/medium/BagsOfRocks.java
+++ src/medium/BagsOfRocks.java
@@ -5,10 +5,15 @@ class BagsOfRocks {
         // Take rocks away from additional rocks
         while (additionalRocks > 0) {
             // Break out of loop if all bags are full
-            for (int bagCapacity : capacity) {
-
+            boolean vacancy = false;
+            for (int i = 0; i < capacity.length; i++) {
+                if (capacity[i] != rocks[i]) {
+                    vacancy = true;
+                }
+                if (!vacancy) {
+                    break;
+                }
+                additionalRocks--;
             }
-            additionalRocks --;
         }
     }
-}
