ed7aec8e749287b9a7bac3cb6b0195a445027711
Justin Sixsmith <justinsixsmith@gmail.com>
2022-12-27 11:10:28 -0600

    creates class to solve Bags Of Rocks daily challenge

---
+++ src/medium/BagsOfRocks.java
@@ -0,0 +1,14 @@
+package medium;
+
+class BagsOfRocks {
+    public int maximumBags(int[] capacity, int[] rocks, int additionalRocks) {
+        // Take rocks away from additional rocks
+        while (additionalRocks > 0) {
+            // Break out of loop if all bags are full
+            for (int bagCapacity : capacity) {
+
+            }
+            additionalRocks --;
+        }
+    }
+}
